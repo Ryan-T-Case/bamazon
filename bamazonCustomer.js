49021a840bacb4c3a91db8de8852408c4f6198d8
Ryan Case <Ryan_Case@outlook.com>
2018-10-27 00:36:39 -0400

    Established a connection to the db and created welcome message

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,35 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+//Here we store the info needed to connect to the bamazon database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Yankees02",
+    database: "bamazon_db"
+});
+
+//Then we establish connection to the datbase
+connection.connect(function(err) {
+    if (err) throw err;
+    //After connection is made successfully, display all items for sale
+    console.log(`
+    ################################################
+    
+    #####    ##   #    #   ##   ######  ####  #    # 
+    #    #  #  #  ##  ##  #  #      #  #    # ##   # 
+    #####  #    # # ## # #    #    #   #    # # #  # 
+    #    # ###### #    # ######   #    #    # #  # # 
+    #    # #    # #    # #    #  #     #    # #   ## 
+    #####  #    # #    # #    # ######  ####  #    #
+    
+    ################################################
+    
+    ##### Welcome to Bamazon, Valued Customer! #####
+    
+    ### We Are Your One Stop Shop for Everything! ###
+    
+    `);
+    
+});
